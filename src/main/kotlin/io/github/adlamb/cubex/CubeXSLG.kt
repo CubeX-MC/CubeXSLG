60ec9849095c8c48654449c06ddfea0d22552d05
Akkariin <fallencrystal@outlook.com>
2026-04-27 16:47:26 +0800

    Template

---
+++ src/main/kotlin/io/github/adlamb/cubex/CubeXSLG.kt
@@ -0,0 +1,14 @@
+package io.github.adlamb.cubex
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CubeXSLG : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
